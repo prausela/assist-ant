5b1eb5a0c30ffca8c60ed72c4549d9e4549c2340
MicaBanfi <micabanfi@hotmail.com>
2018-11-08 16:46:36 -0300

    no stage nada antes

--- mobile/app/src/main/java/com/assist/home/assisthome/DeviceAC.java
+++ mobile/app/src/main/java/com/assist/home/assisthome/DeviceAC.java
@@ -1,25 +1,37 @@
 package com.assist.home.assisthome;
 
 import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class DeviceAC extends AppCompatActivity {
 
-    Button f25,f50,f75,f100,fauto;
+    ImageButton ac_power;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_ac);
-
-//        b1=(Button)findViewById(R.id.b1);
-//        b1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                b1.
-//            }
-//        });
+        ac_power = (ImageButton) findViewById(R.id.ac_power);
+        ac_power.setOnClickListener(ac_power_Handler);
     }
+    View.OnClickListener ac_power_Handler=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if ("ac_power_on".equals(ac_power.getTag())) {
+                ac_power.setBackgroundResource(R.drawable.ac_power_off);
+                ac_power.setTag("ac_power_off");
+            }
+            else {
+                ac_power.setBackgroundResource(R.drawable.ac_power_on);
+                ac_power.setTag("ac_power_on");
+            }
+        }
+    };
+
 }
