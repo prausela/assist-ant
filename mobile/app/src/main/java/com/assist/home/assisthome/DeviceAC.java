8aa4641bf1eefbaa9fbea09a043699e3ba8f0386
MicaBanfi <micabanfi@hotmail.com>
2018-11-09 19:49:20 -0300

    lindo ac y fridge

--- mobile/app/src/main/java/com/assist/home/assisthome/DeviceAC.java
+++ mobile/app/src/main/java/com/assist/home/assisthome/DeviceAC.java
@@ -8,10 +8,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class DeviceAC extends AppCompatActivity {
+public class DeviceAC extends AppActivity {
 
     ImageButton ac_power;
+    ImageButton ac_mode;
+    ImageButton ac_fan_speed_low, ac_fan_speed_mid, ac_fan_speed_high, ac_fan_speed_auto;
+    ImageButton ac_mode_heat, ac_mode_cold, ac_mode_auto;
+    TextView ac_temp;
+    Button ac_temp_up, ac_temp_down;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +26,186 @@ public class DeviceAC extends AppCompatActivity {
         setContentView(R.layout.activity_device_ac);
         ac_power = (ImageButton) findViewById(R.id.ac_power);
         ac_power.setOnClickListener(ac_power_Handler);
+
+        ac_temp_up = (Button) findViewById(R.id.ac_temp_up);
+        ac_temp_down = (Button) findViewById(R.id.ac_temp_down);
+        ac_temp = (TextView) findViewById(R.id.ac_temp);
+
+        ac_temp_up.setOnClickListener(ac_temp_up_Handler);
+        ac_temp_down.setOnClickListener(ac_temp_down_Handler);
+
+        ac_fan_speed_low = (ImageButton) findViewById(R.id.ac_fan_speed_low);
+        ac_fan_speed_mid = (ImageButton) findViewById(R.id.ac_fan_speed_mid);
+        ac_fan_speed_high = (ImageButton) findViewById(R.id.ac_fan_speed_high);
+        ac_fan_speed_auto = (ImageButton) findViewById(R.id.ac_fan_speed_auto);
+
+        ac_fan_speed_low.setOnClickListener(ac_fan_speed_low_Handler);
+        ac_fan_speed_mid.setOnClickListener(ac_fan_speed_mid_Handler);
+        ac_fan_speed_high.setOnClickListener(ac_fan_speed_high_Handler);
+        ac_fan_speed_auto.setOnClickListener(ac_fan_speed_auto_Handler);
+
+        ac_mode_heat = (ImageButton) findViewById(R.id.ac_mode_heat);
+        ac_mode_cold = (ImageButton) findViewById(R.id.ac_mode_cold);
+        ac_mode_auto = (ImageButton) findViewById(R.id.ac_mode_auto);
+
+        ac_mode_heat.setOnClickListener(ac_mode_heat_Handler);
+        ac_mode_cold.setOnClickListener(ac_mode_cold_Handler);
+        ac_mode_auto.setOnClickListener(ac_mode_auto_Handler);
+
     }
-    View.OnClickListener ac_power_Handler=new View.OnClickListener() {
+
+    View.OnClickListener ac_temp_up_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if("ac_power_on".equals(ac_power.getTag())) {
+                int temp = Integer.valueOf(ac_temp.getText().toString());
+                ac_temp.setText(Integer.toString(temp + 1));
+            }
+        }
+    };
+
+    View.OnClickListener ac_temp_down_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if ("ac_power_on".equals(ac_power.getTag())) {
+                int temp = Integer.valueOf(ac_temp.getText().toString());
+                ac_temp.setText(Integer.toString(temp - 1));
+            }
+        }
+    };
+
+    View.OnClickListener ac_mode_auto_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if("ac_power_on".equals(ac_power.getTag())) {
+                if ("ac_mode_auto_inactive".equals(ac_mode_auto.getTag())) {
+                    ac_mode_auto.setBackgroundResource(R.drawable.ac_fan_auto_active);
+                    ac_mode_auto.setTag("ac_mode_auto_active");
+                    ac_mode_heat.setBackgroundResource(R.drawable.ac_heat_mode_inactive);
+                    ac_mode_heat.setTag("ac_mode_heat_inactive");
+                    ac_mode_cold.setBackgroundResource(R.drawable.ac_cool_mode_inactive);
+                    ac_mode_cold.setTag("ac_mode_cold_inactive");
+                }
+            }
+        }
+    };
+
+    View.OnClickListener ac_mode_cold_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if("ac_power_on".equals(ac_power.getTag())) {
+                if ("ac_mode_cold_inactive".equals(ac_mode_cold.getTag())) {
+                    ac_mode_cold.setBackgroundResource(R.drawable.ac_cool_mode_active);
+                    ac_mode_cold.setTag("ac_mode_cold_active");
+                    ac_mode_heat.setBackgroundResource(R.drawable.ac_heat_mode_inactive);
+                    ac_mode_heat.setTag("ac_mode_heat_inactive");
+                    ac_mode_auto.setBackgroundResource(R.drawable.ac_fan_auto_inactive);
+                    ac_mode_auto.setTag("ac_mode_auto_inactive");
+                }
+            }
+        }
+    };
+
+    View.OnClickListener ac_mode_heat_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if("ac_power_on".equals(ac_power.getTag())) {
+                if ("ac_mode_heat_inactive".equals(ac_mode_heat.getTag())) {
+                    ac_mode_heat.setBackgroundResource(R.drawable.ac_heat_mode_active);
+                    ac_mode_heat.setTag("ac_mode_heat_active");
+                    ac_mode_cold.setBackgroundResource(R.drawable.ac_cool_mode_inactive);
+                    ac_mode_cold.setTag("ac_mode_cold_inactive");
+                    ac_mode_auto.setBackgroundResource(R.drawable.ac_fan_auto_inactive);
+                    ac_mode_auto.setTag("ac_mode_auto_inactive");
+                }
+            }
+        }
+    };
+
+    View.OnClickListener ac_fan_speed_low_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if("ac_power_on".equals(ac_power.getTag())) {
+                if ("ac_fan_speed_low_inactive".equals(ac_fan_speed_low.getTag())) {
+                    ac_fan_speed_low.setBackgroundResource(R.drawable.ac_fan_low_active);
+                    ac_fan_speed_low.setTag("ac_fan_speed_low_active");
+                    ac_fan_speed_mid.setBackgroundResource(R.drawable.ac_fan_mid_inactive);
+                    ac_fan_speed_mid.setTag("ac_fan_speed_mid_inactive");
+                    ac_fan_speed_high.setBackgroundResource(R.drawable.ac_fan_high_inactive);
+                    ac_fan_speed_high.setTag("ac_fan_speed_high_inactive");
+                    ac_fan_speed_auto.setBackgroundResource(R.drawable.ac_fan_auto_inactive);
+                    ac_fan_speed_auto.setTag("ac_fan_speed_auto_inactive");
+                }
+            }
+        }
+    };
+    View.OnClickListener ac_fan_speed_mid_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if("ac_power_on".equals(ac_power.getTag())) {
+                if ("ac_fan_speed_mid_inactive".equals(ac_fan_speed_mid.getTag())) {
+                    ac_fan_speed_mid.setBackgroundResource(R.drawable.ac_fan_mid_active);
+                    ac_fan_speed_mid.setTag("ac_fan_speed_mid_active");
+                    ac_fan_speed_low.setBackgroundResource(R.drawable.ac_fan_low_inactive);
+                    ac_fan_speed_low.setTag("ac_fan_speed_low_inactive");
+                    ac_fan_speed_high.setBackgroundResource(R.drawable.ac_fan_high_inactive);
+                    ac_fan_speed_high.setTag("ac_fan_speed_high_inactive");
+                    ac_fan_speed_auto.setBackgroundResource(R.drawable.ac_fan_auto_inactive);
+                    ac_fan_speed_auto.setTag("ac_fan_speed_auto_inactive");
+
+                }
+            }
+        }
+    };
+    View.OnClickListener ac_fan_speed_high_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if("ac_power_on".equals(ac_power.getTag())) {
+                if ("ac_fan_speed_high_inactive".equals(ac_fan_speed_high.getTag())) {
+                    ac_fan_speed_high.setBackgroundResource(R.drawable.ac_fan_high_active);
+                    ac_fan_speed_high.setTag("aac_fan_speed_high_active");
+                    ac_fan_speed_mid.setBackgroundResource(R.drawable.ac_fan_mid_inactive);
+                    ac_fan_speed_mid.setTag("ac_fan_speed_mid_inactive");
+                    ac_fan_speed_low.setBackgroundResource(R.drawable.ac_fan_low_inactive);
+                    ac_fan_speed_low.setTag("ac_fan_speed_low_inactive");
+                    ac_fan_speed_auto.setBackgroundResource(R.drawable.ac_fan_auto_inactive);
+                    ac_fan_speed_auto.setTag("ac_fan_speed_auto_inactive");
+
+                }
+            }
+        }
+    };
+    View.OnClickListener ac_fan_speed_auto_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if ("ac_power_on".equals(ac_power.getTag())) {
+                if ("ac_fan_speed_auto_inactive".equals(ac_fan_speed_auto.getTag())) {
+                    ac_fan_speed_auto.setBackgroundResource(R.drawable.ac_fan_auto_active);
+                    ac_fan_speed_auto.setTag("aac_fan_speed_auto_active");
+                    ac_fan_speed_mid.setBackgroundResource(R.drawable.ac_fan_mid_inactive);
+                    ac_fan_speed_mid.setTag("ac_fan_speed_mid_inactive");
+                    ac_fan_speed_low.setBackgroundResource(R.drawable.ac_fan_low_inactive);
+                    ac_fan_speed_low.setTag("ac_fan_speed_low_inactive");
+                    ac_fan_speed_high.setBackgroundResource(R.drawable.ac_fan_high_inactive);
+                    ac_fan_speed_high.setTag("ac_fan_speed_high_inactive");
+
+                }
+            }
+        }
+    };
+
+    View.OnClickListener ac_power_Handler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             if ("ac_power_on".equals(ac_power.getTag())) {
                 ac_power.setBackgroundResource(R.drawable.ac_power_off);
                 ac_power.setTag("ac_power_off");
-            }
-            else {
+            } else {
                 ac_power.setBackgroundResource(R.drawable.ac_power_on);
                 ac_power.setTag("ac_power_on");
             }
         }
     };
+
 
 }
---
+++ mobile/app/src/main/java/com/assist/home/assisthome/DeviceFridge.java
@@ -0,0 +1,119 @@
+package com.assist.home.assisthome;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class DeviceFridge extends AppActivity {
+    ImageButton def, party, trip;
+    TextView f_temp, r_temp;
+    Button f_temp_up, f_temp_down, r_temp_up, r_temp_down;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_ac);
+        f_temp_up = (Button) findViewById(R.id.fridge_temp_up);
+        f_temp_down = (Button) findViewById(R.id.fridge_temp_down);
+        f_temp = (TextView) findViewById(R.id.fridge_temp);
+        r_temp_up = (Button) findViewById(R.id.refri_temp_up);
+        r_temp_down = (Button) findViewById(R.id.refri_temp_down);
+        r_temp = (TextView) findViewById(R.id.refri_temp);
+
+        f_temp_up.setOnClickListener(f_temp_up_Handler);
+        f_temp_down.setOnClickListener(f_temp_down_Handler);
+        r_temp_up.setOnClickListener(r_temp_up_Handler);
+        r_temp_down.setOnClickListener(r_temp_down_Handler);
+
+        def = (ImageButton) findViewById(R.id.fridge_def);
+        party = (ImageButton) findViewById(R.id.fridge_party);
+        trip = (ImageButton) findViewById(R.id.fridge_trip);
+
+        def.setOnClickListener(def_Handler);
+        party.setOnClickListener(party_Handler);
+        trip.setOnClickListener(trip_Handler);
+
+    }
+
+    View.OnClickListener def_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if ("fridge_def_inactive".equals(def.getTag())) {
+                def.setBackgroundResource(R.drawable.fridge_def_active);
+                def.setTag("fridge_def_active");
+                party.setBackgroundResource(R.drawable.fridge_party_inactive);
+                party.setTag("fridge_party_inactive");
+                trip.setBackgroundResource(R.drawable.fridge_trip_inactive);
+                trip.setTag("fridge_trip_inactive");
+            }
+        }
+
+    };
+
+    View.OnClickListener party_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if ("fridge_party_inactive".equals(party.getTag())) {
+                party.setBackgroundResource(R.drawable.fridge_party_active);
+                party.setTag("fridge_def_active");
+                def.setBackgroundResource(R.drawable.fridge_def_inactive);
+                def.setTag("fridge_def_inactive");
+                trip.setBackgroundResource(R.drawable.fridge_trip_inactive);
+                trip.setTag("fridge_trip_inactive");
+            }
+        }
+
+    };
+    View.OnClickListener trip_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if ("fridge_trip_inactive".equals(trip.getTag())) {
+                trip.setBackgroundResource(R.drawable.fridge_trip_active);
+                trip.setTag("fridge_trip_active");
+                party.setBackgroundResource(R.drawable.fridge_party_inactive);
+                party.setTag("fridge_party_inactive");
+                def.setBackgroundResource(R.drawable.fridge_def_inactive);
+                def.setTag("fridge_def_inactive");
+            }
+        }
+
+    };
+
+    View.OnClickListener f_temp_up_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int tempF = Integer.valueOf(f_temp.getText().toString());
+            f_temp.setText(Integer.toString(tempF + 1));
+        }
+    };
+
+    View.OnClickListener f_temp_down_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int tempF = Integer.valueOf(f_temp.getText().toString());
+            f_temp.setText(Integer.toString(tempF - 1));
+        }
+    };
+
+    View.OnClickListener r_temp_down_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int tempR = Integer.valueOf(r_temp.getText().toString());
+            r_temp.setText(Integer.toString(tempR - 1));
+        }
+    };
+    View.OnClickListener r_temp_up_Handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int tempR = Integer.valueOf(r_temp.getText().toString());
+            r_temp.setText(Integer.toString(tempR + 1));
+        }
+    };
+
+
+}
