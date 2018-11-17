553561f773556a36c2364b3262e4f597e6af80c5
MicaBanfi <micabanfi@hotmail.com>
2018-11-17 16:20:31 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ mobile/app/src/main/java/com/assist/home/assisthome/Settings.java
@@ -0,0 +1,40 @@
+package com.assist.home.assisthome;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Settings extends AppActivity implements AdapterView.OnItemSelectedListener  {
+    CheckBox active,inactive;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_devices_cards);
+        super.setContent(R.layout.activity_settings,"Ajustes");
+
+        Spinner spinner=findViewById(R.id.spinner_language);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.languageOp,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text=parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+        
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
+
